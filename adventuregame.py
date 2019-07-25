148320de4de3ba80270fcb1f107d146f58c3dcbc
yonatank21-meet <yonatank21@meet.mit.edu>
2019-07-25 14:29:46 +0300

    Stopping here

---
+++ adventuregame.py
@@ -0,0 +1,16 @@
+print("Hey there and welcome to the game!")
+name = input("How would you like to name your charapter?")
+print("nice to meet you " + name +", I hope you survive!")
+print("Lets begin!")
+#question one:
+print("Your on your way to work and you see a sick old lady sitting on the side of the road, what do you do?")
+#answers
+print("1.keep walking, she is not your problem!")
+print("2. go to her and ask her if everything is ok.")
+answer = input("what are you doing? (1 or 2):")
+if answer == "1": 
+    print("you get ran over by a drunk t-rex and you die, karma is a bitch!")
+elif answer == "2":
+    print ("correct")
+ 
+    
